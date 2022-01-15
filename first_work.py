966de36e653347dc700830ff5ccd11fe8f5022a5
Godswill Sedinam Lanyo <godswilllanyo@gmail.com>
2022-01-15 09:34:46 -0800

    two function calculator

---
+++ first_work.py
@@ -0,0 +1,41 @@
+def add_values(a , b):
+    print(a + b)
+
+def sub_values(a , b):
+    print(a - b)
+
+def two_func_calc():
+    a = 10
+    b = 20
+    operator = input('Enter the operator')
+    if  operator == '+':
+        add_values(a , b)
+    elif operator =='-':
+        sub_values(a , b)
+
+#two_func_calc()
+add_values(11 , 12)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
